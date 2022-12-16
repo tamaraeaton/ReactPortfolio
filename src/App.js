1a16376e9e40ad571e1f5a8ffa1e6163dda10a17
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-12-16 14:25:15 -0700

    changed basename

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/ReactPortfolio">
       <Routes />
     </BrowserRouter>
   );
